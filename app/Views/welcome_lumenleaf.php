6775d71300c2902d23b7789786e4cf901c2824e8
Matheus Felipe Lazarini Braghin <matheusfelipelazarini4@gmail.com>
2024-11-26 13:37:20 -0300

    Commit inicia

---
+++ app/Views/welcome_lumenleaf.php
@@ -0,0 +1,32 @@
+<div class="container-fluid holder-image">
+    <div class="box">
+        <h1>L U M E N L E A F</h1>
+    </div>
+</div>
+
+<div class="information-lumenleaf" style="padding-top: 4rem;">
+    <h4 class="text-center">LumenLeaf has been created with the objective of us is <br> make you more agile in taking notes.</h4>
+</div>
+
+<div class="content-about">
+
+    <div class="aside-left" style="float:left;">
+        <h4 class="text-center">
+            <img src="<?=base_url('content/icons/lupa.png'); ?>" width="120px" height="120px">
+            Find things fast<br>
+
+            Search and filter for what you want.
+        </h4>
+    </div>
+
+    <img src="<?=base_url('content/icons/leaf-separator.png')?>" alt="" width="50px" height="50px">
+    
+<div class="aside-right" style="float:right;">
+    <h4 class="text-center">
+        <img src="<?=base_url('content/icons/attach-removebg-preview.png'); ?>" width="120px" height="120px" style="rotate: 35deg;">Every file in one<br>
+
+        Possibility of importing files as PDFs, <br>images, audio and documents.
+    </h4>
+</div>
+
+</div>
